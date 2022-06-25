a90d2388f95cef4e712b1cd5fd893550366062b3
Luke O'Brien <lukeobrien02@gmail.com>
2022-06-25 15:24:55 +0100

    [TW-26] fixed issue in secure router by changing position of next() comments now working

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,27 +2,30 @@ import Sound from '../models/soundModel.js'
 
 async function createComment(req, res) {
   try {
-    //! We need to provide the pokemon ID we're commenting on.
+
     const soundById = req.params.soundId
-    //! We also need to get the user/user ID for the user commenting.
+    console.log(soundById)
+    
     const user = req.currentUser
-    //! Grab the info from the request body.
+    console.log(user)
+    
     const comment = req.body
-    // ! Get the pokemon we're comment on.
+    console.log(comment)
+  
     const sound = await Sound.findById(soundById)
-    // ! Handle it if no pokemon is found
+    console.log(sound)
+
     if (!sound) {
       return res.json({ message: 'No sound file found with this ID' })
     }
-    // ! Push the new comment to the comments array
-    comment.user = user
-    // ! Pushing our new comment to this pokemon does
-    // ! NOT update it in the database YET. 
-    sound.comments.push(comment)
 
-    // ! So we need to save it to the database.
+    comment.user = user
+
+    sound.comments.push(comment)
+    console.log(comment)
+
     const savedSound = await sound.save()
-    // ! Sending back the comment
+
     res.json(savedSound)
   } catch (err) {
     res.json({ message: "There was a problem commenting" })
