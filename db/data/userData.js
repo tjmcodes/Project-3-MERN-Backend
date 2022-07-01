e33452ec87bbe44b3f5f7b0c1eacf02103b052ff
Laleh <laleh.shahidi78@gmail.com>
2022-07-01 17:23:37 +0100

    add user profiles to user Data

--- db/data/userData.js
+++ db/data/userData.js
@@ -1,9 +1,50 @@
 export default [
   {
-    "username": "laleh",
-    "password": "Whisperers4!",
-    "passwordConfirmation": "Whisperers4!",
-    "email": "whisperers@whisperers.com",
+    "username": "Doggy",
+    "password": "Doggy!22",
+    "passwordConfirmation": "Doggy!22",
+    "email": "doggy@doggy.com",
     "image": "https://res.cloudinary.com/tjmcodes/image/upload/v1656610419/my_found_sounds_pics/srywvdlgnsykvpdwnvuw.jpg",  
+  },
+
+  {
+    "username": "Lilith",
+    "password": "Lilith!22",
+    "passwordConfirmation": "Lilith!22",
+    "email": "lilith@lilith.com",
+    "image": "https://res.cloudinary.com/tjmcodes/image/upload/v1656684466/my_found_sounds_pics/rcrm13doxkggeslt3hye.jpg",  
+  },
+
+  {
+    "username": "Milos",
+    "password": "Milos!22",
+    "passwordConfirmation": "Milos!22",
+    "email": "milos@milos.com",
+    "image": "https://res.cloudinary.com/tjmcodes/image/upload/v1656685150/my_found_sounds_pics/aam1zfiktc0l1gp6weei.jpg",  
+  },
+
+  {
+    "username": "Jasmin",
+    "password": "Jasmin!22",
+    "passwordConfirmation": "Jasmin!22",
+    "email": "jasmin@jasmin.com",
+    "image": "https://res.cloudinary.com/tjmcodes/image/upload/v1656685261/my_found_sounds_pics/bir5rm3bgymeelhs9byw.jpg",  
+  },
+
+  {
+    "username": "Hanna",
+    "password": "Hanna!22",
+    "passwordConfirmation": "Hanna!22",
+    "email": "hanna@hanna.com",
+    "image": "https://res.cloudinary.com/tjmcodes/image/upload/v1656685456/my_found_sounds_pics/cyb9xdry9wmkxxlahisk.jpg",  
+  },
+
+  {
+    "username": "Lucas",
+    "password": "Lucas!22",
+    "passwordConfirmation": "Lucas!22",
+    "email": "lucas@lucas.com",
+    "image": "https://res.cloudinary.com/tjmcodes/image/upload/v1656684992/my_found_sounds_pics/ao4xgnnekoe8de8mjuwj.jpg",  
   }
+
 ]
