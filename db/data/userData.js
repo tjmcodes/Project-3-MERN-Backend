dc41ea07fafbb433b811848c9988bee4ca73d46d
Teresa Morini <tjmcodes@gmail.com>
2022-07-01 15:52:25 +0100

    dog image included inuser seed data

--- db/data/userData.js
+++ db/data/userData.js
@@ -4,6 +4,6 @@ export default [
     "password": "Whisperers4!",
     "passwordConfirmation": "Whisperers4!",
     "email": "whisperers@whisperers.com",
-    "image": "https://res.cloudinary.com/dy4gabnho/image/upload/v1655660650/pets/my_favorite/axjizpfu7eqzg0uqcigf.jpg",  
+    "image": "https://res.cloudinary.com/tjmcodes/image/upload/v1656610419/my_found_sounds_pics/srywvdlgnsykvpdwnvuw.jpg",  
   }
 ]
