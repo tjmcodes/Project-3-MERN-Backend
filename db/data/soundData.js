969493ee436208bcaa487103e5d0adf0b8ad6256
Teresa Morini <tjmcodes@gmail.com>
2022-07-12 14:26:55 +0100

    commented out sound data posts

--- db/data/soundData.js
+++ db/data/soundData.js
@@ -1,29 +1,29 @@
 export default [
   {
-    fileName: 'bells 1',
-    caption: 'this is a caption',
-    hashtag: [ 'ambient', 'bells', 'electronica' ],
-    category: 'machines',
-    subCategory: 'power drills',
-    url: 'https://res.cloudinary.com/tjmcodes/video/upload/v1655481830/sea.wav',
-    image: 'https://res.cloudinary.com/tjmcodes/video/upload/h_200,w_500,fl_waveform/v1656611932/my_found_sounds/ivtjkcpiijzrqy8upvke.png',
-  },
-  {
-    fileName: 'bells 2',
-    caption: 'this is a caption2',
-    hashtag: [ 'ambient', 'bells', 'electronica' ],
+    fileName: 'sea',
+    caption: 'Sounds captured at shoreham beach',
+    hashtag: [ 'ambience', 'stones', 'sea' ],
     category: 'nature',
-    subCategory: 'countryside',
+    subCategory: 'sea',
     url: 'https://res.cloudinary.com/tjmcodes/video/upload/v1655481830/sea.wav',
-    image: 'https://res.cloudinary.com/tjmcodes/video/upload/h_200,w_500,fl_waveform/v1656611932/my_found_sounds/ivtjkcpiijzrqy8upvke.png',
-  },
-  {
-    fileName: 'bells 3',
-    caption: 'this is a caption3',
-    hashtag: [ 'ambient', 'bells', 'electronica' ],
-    category: 'animals',
-    subCategory: 'birds',
-    url: 'https://res.cloudinary.com/tjmcodes/video/upload/v1655481830/sea.wav',
-    image: 'https://res.cloudinary.com/tjmcodes/video/upload/h_200,w_500,fl_waveform/v1656611932/my_found_sounds/ivtjkcpiijzrqy8upvke.png',
+    image: 'https://res.cloudinary.com/tjmcodes/video/upload/h_200,w_500,fl_waveform//v1655481830/sea.png',
   }
+  // {
+  //   fileName: 'bells 2',
+  //   caption: 'this is a caption2',
+  //   hashtag: [ 'ambient', 'bells', 'electronica' ],
+  //   category: 'nature',
+  //   subCategory: 'countryside',
+  //   url: 'https://res.cloudinary.com/tjmcodes/video/upload/v1655481830/sea.wav',
+  //   image: 'https://res.cloudinary.com/tjmcodes/video/upload/h_200,w_500,fl_waveform/v1656611932/my_found_sounds/ivtjkcpiijzrqy8upvke.png',
+  // },
+  // {
+  //   fileName: 'bells 3',
+  //   caption: 'this is a caption3',
+  //   hashtag: [ 'ambient', 'bells', 'electronica' ],
+  //   category: 'animals',
+  //   subCategory: 'birds',
+  //   url: 'https://res.cloudinary.com/tjmcodes/video/upload/v1655481830/sea.wav',
+  //   image: 'https://res.cloudinary.com/tjmcodes/video/upload/h_200,w_500,fl_waveform/v1656611932/my_found_sounds/ivtjkcpiijzrqy8upvke.png',
+  // }
 ]
